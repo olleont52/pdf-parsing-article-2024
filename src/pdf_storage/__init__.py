0b77047c95773960ea14d9c60bffd847ed37d8a4
olleont52 <leontyev.ol2@gmail.com>
2024-03-26 00:11:35 +0300

    Add figure report

--- src/pdf_storage/__init__.py
+++ src/pdf_storage/__init__.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 PDF_STORAGE_PATH = Path(__file__).resolve().parent
 
+png_image = str(PDF_STORAGE_PATH / 'little_lamb.png')
 table_report_file_path = str(PDF_STORAGE_PATH / 'table_report.pdf')
+figures_file_path = str(PDF_STORAGE_PATH / 'figures.pdf')
