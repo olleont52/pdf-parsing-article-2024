450f2ec6a3e737ed6002866fbfc93e16010f7880
olleont52 <leontyev.ol2@gmail.com>
2024-03-25 23:36:15 +0300

    Add page stream saver

---
+++ src/parsereports/save_page_stream.py
@@ -0,0 +1,28 @@
+from parsereports.basicparsing import BasicPdfParser
+from pdf_storage import table_report_file_path
+
+INPUT_FILE_PATH = table_report_file_path
+PAGE_INDEX = 0
+SAVED_FILE_PATH = "stream.txt"
+
+
+def main():
+    input_file_path = input(f"PDF file path (default=\"{INPUT_FILE_PATH}\"): ") or INPUT_FILE_PATH
+    page_index_str = input(f"Page number, starting from 0 (default={PAGE_INDEX}): ") or str(PAGE_INDEX)
+    page_index = int(page_index_str)
+    saved_file_path = input(f"File name/path to save the page (default=\"{SAVED_FILE_PATH}\"): ") or SAVED_FILE_PATH
+
+    parser = BasicPdfParser(input_file_path)
+    pdf_doc = parser.pq.doc
+    page_dict = pdf_doc.catalog["Pages"].resolve()["Kids"][page_index].resolve()
+    page_content_stream = page_dict["Contents"].resolve()
+
+    with open(saved_file_path, 'wb') as f:
+        f.write(page_content_stream.data)
+        f.flush()
+
+    print("Saved " + saved_file_path)
+
+
+if __name__ == '__main__':
+    main()
