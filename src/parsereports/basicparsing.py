6b36678cf65a6ce7df591294286f7769fcb45967
olleont52 <leontyev.ol2@gmail.com>
2024-03-29 17:27:51 +0300

    Add a benchmarking script

--- src/parsereports/basicparsing.py
+++ src/parsereports/basicparsing.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any, Dict
 
 from pdfminer.layout import LTPage
 from pdfquery import PDFQuery
@@ -6,16 +6,24 @@ from pdfquery.pdfquery import LayoutElement
 
 
 class BasicPdfParser:
-    def __init__(self, pdf_file_path: str):
+    def __init__(
+            self,
+            pdf_file_path: str,
+            pq_params: Optional[Dict[str, Any]] = None
+    ):
         self._file_path = pdf_file_path
         self._pq: Optional[PDFQuery] = None
+        self._pq_params = pq_params or {}
 
     @property
     def pq(self) -> PDFQuery:
-        if self._pq is None:
-            self._pq = PDFQuery(self._file_path)
-            self._pq.load()
+        self.init_pq()
         return self._pq
+
+    def init_pq(self) -> None:
+        if self._pq is None:
+            self._pq = PDFQuery(self._file_path, **self._pq_params)
+            self._pq.load()
 
     def close(self) -> None:
         pq = self._pq
